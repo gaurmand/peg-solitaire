19f808ba033e72d9824df81106306047f4865638
gaurmand <gaurav_amwan@hotmail.com>
2021-04-26 01:08:11 -0700

    Move PegSolitaireBoardInitializer

--- src/movecomputingpegsolitaire.js
+++ src/movecomputingpegsolitaire.js
@@ -1,12 +1,109 @@
-const {PegSolitaireBoardInitializer} = require('./pegsolitaireboardinitializer');
+class PegSolitaireBoardInitializer {
+
+    static isValidBoardString(str) {
+        if(!this.isBoardStringValidSize(str)) {
+            return false;
+        }
+
+        if(!this.doesBoardStringContainValidChars(str)) {
+            return false;
+        }
+
+        if(this.doesBoardStringBeginOrEndWithDelimeters(str)) {
+            return false;
+        }
+
+        if(!this.doesBoardStringHaveEqualRowLengths(str)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    static isBoardStringValidSize(str) {
+        return str && str.length > 0;
+    }
+
+    static doesBoardStringContainValidChars(str) {
+        return /^[\d.o/]+$/.test(str); //only contains numbers, ".", "o", and "/" characters
+    }
+
+    static doesBoardStringBeginOrEndWithDelimeters(str) {
+        return str[0] === "/" || str[str.length-1] === "/";
+    }
+
+    static doesBoardStringHaveEqualRowLengths(str) {
+        let rowLengths = this.getRowLengthsFromBoardString(str);
+        return rowLengths.every(length => length === rowLengths[0]);
+    }
+
+    static getRowLengthsFromBoardString(str) {
+        let rows = str.split("/");
+        let rowLengths = rows.map(rowStr => this.getRowLengthFromRowString(rowStr));
+        return rowLengths;
+    }
+
+    static getRowLengthFromRowString(rowStr) {
+        let isDigit = char => /^\d+$/.test(char);
+
+        let length = 0;
+        [...rowStr].forEach(char => {
+            if(isDigit(char)) {
+                length += parseInt(char);
+            } else {
+                length++;
+            }
+        });
+        return length;
+    }
+
+    static getBoardFromString(str) {
+        let isDigit = char => /^\d+$/.test(char);
+
+        let board = [[]];
+        let holes = [];
+        let numValidPositions = 0;
+        let row = 0;
+        let col = 0;
+
+        [...str].forEach(char => {
+            if(isDigit(char)) {
+                let num = parseInt(char);
+                for(let i = col; i < col + num; i++) {
+                    board[row].push(" ");
+                }
+                col += num;
+            } else {
+                switch(char) {
+                    case "o":
+                        holes.push([row, col]);
+                    case ".":
+                        board[row].push(char);
+                        col++
+                        numValidPositions++;
+                        break; 
+                    case "/":
+                        row++;
+                        col = 0;
+                        board.push([]);
+                        break;
+                    default:
+                        throw new Error("Invalid initialization string");
+                }
+            }
+        });
+
+        return {board, holes, numValidPositions};
+    }
+};
 
 class PegSolitaireBoard {
     constructor(str) {
-        if(!PegSolitaireBoardInitializer.isValidInitString(str)) {
+        if(!PegSolitaireBoardInitializer.isValidBoardString(str)) {
             throw new Error("Invalid initialization string");
         }
 
-        let res = PegSolitaireBoardInitializer.getBoardFromInitString(str);
+        let res = PegSolitaireBoardInitializer.getBoardFromString(str);
         this.board = res.board;
         this.holes = res.holes;
         this.numValidPositions = res.numValidPositions;
